29c945dfabe2f38b1ff9c532392b1b5d6e42eef8
kimdw524 <kimdw524@naver.com>
2024-06-18 01:13:11 +0900

    init

---
+++ .storybook/preview.ts
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { ThemeProvider } from '@emotion/react';
+import Theme from '../src/themes/theme';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeFromJSXProvider({
+      themes: {
+        light: Theme,
+      },
+      defaultTheme: 'light',
+      Provider: ThemeProvider,
+    }),
+  ],
+};
+
+export default preview;
