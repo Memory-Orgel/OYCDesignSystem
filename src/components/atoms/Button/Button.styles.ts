add36c4eda7f72ec96f33d591eb3373de3751c5d
kimdw524 <kimdw524@naver.com>
2024-07-05 02:21:33 +0900

    style: css 줄바꿈 제거

--- src/components/atoms/Button/Button.styles.ts
+++ src/components/atoms/Button/Button.styles.ts
@@ -8,24 +8,19 @@ export const base = (theme: Theme, size: Size) => css`
   box-sizing: border-box;
   border: 0;
   border-radius: ${theme.borderRadius.sm};
-
   font-size: ${theme.typography.fontSize[size]};
   font-weight: ${theme.typography.fontWeight.medium};
-
   cursor: pointer;
 `;
 
 export const variants: Record<ButtonVariant, (theme: Theme, palette: PaletteColor) => SerializedStyles> = {
   contained: (theme: Theme, palette: PaletteColor) => css`
     background-color: ${palette.main};
-
     color: ${palette.contrastText};
   `,
   outlined: (theme: Theme, palette: PaletteColor) => css`
     border: 1px solid ${palette.main};
-
     background-color: ${theme.colors.background};
-
     color: ${palette.main};
   `,
 };
