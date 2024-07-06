72bed3e20fb8848d3c99ea3b8fef7ddb496d781c
kimdw524 <kimdw524@naver.com>
2024-07-06 17:39:47 +0900

    feat: add xl size

--- src/themes/themeBase.ts
+++ src/themes/themeBase.ts
@@ -1,6 +1,6 @@
 import colorPalette from './colorPalette';
 
-export type Size = 'sm' | 'md' | 'lg';
+export type Size = 'sm' | 'md' | 'lg' | 'xl';
 export type Weight = 'light' | 'regular' | 'medium' | 'semiBold' | 'bold' | 'extraBold';
 
 export type ThemeBaseType = {
@@ -19,17 +19,20 @@ const ThemeBase: ThemeBaseType = {
     sm: '0.75rem',
     md: '0.875rem',
     lg: '1rem',
+    xl: '1.25rem',
   },
   borderRadius: {
     sm: '0.25rem',
     md: '0.375rem',
     lg: '0.5rem',
+    xl: '0.625rem',
   },
   typography: {
     fontSize: {
       sm: '0.8125rem',
       md: '0.875rem',
       lg: '1rem',
+      xl: '1.25rem',
     },
     fontWeight: {
       light: 300,
