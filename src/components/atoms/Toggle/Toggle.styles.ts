d053fd8e0f692550f819bb693aed6150eacd965c
bluewhaleyh <bluewhaleyh@gmail.com>
2024-07-09 08:48:35 +0900

    feat: css 속성 컨벤션에 맞춰 수정
    css 속성 컨벤션에 맞춰 순서 변경

--- src/components/atoms/Toggle/Toggle.styles.ts
+++ src/components/atoms/Toggle/Toggle.styles.ts
@@ -3,20 +3,20 @@ import { PaletteColor } from '../../../themes/lightTheme';
 
 export const base = (theme: Theme, palette: PaletteColor) => css`
   position: relative;
-  padding: 0.125rem 0;
-  border-radius: 1rem;
   width: 2.125rem;
   height: 1rem;
+  padding: 0.125rem 0;
+  border-radius: 1rem;
   background-color: ${palette.main};
   transition: background-color 300ms;
 `;
 
 export const thumb = (value: boolean) => css`
   position: absolute;
+  left: ${value ? '0.125rem' : '1rem'};
   width: 1rem;
   height: 1rem;
   border-radius: 0.5rem;
   background-color: white;
-  left: ${value ? '0.125rem' : '1rem'};
   transition: left 300ms ease-in-out;
 `;
